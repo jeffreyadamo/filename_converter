d46555422e5407bee7d766f69969e98ef63b5d98
jeffreyadamo <jeffreyadamo@gmail.com>
2021-10-29 00:30:52 -0700

    ensuring the readme.md doesn't change either

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const files = readdirSync(pathToDirectory);
 
 // Loop through each file that was retrieved, take first 7 letters only and keep .txt filetype
 files.forEach(file => {
-  if (file != "index.js"){
-    const oldPath = pathToDirectory + `/${file}`;
-    const newPath = pathToDirectory + `/${file.slice(0, 7)}` + '.txt';
-    rename(
-      oldPath,
-      newPath,
-      err => console.log(err)
-    )
-  }
+  if (file === "index.js" )
+  {} else if (file !="README.md") {
+      const oldPath = pathToDirectory + `/${file}`;
+      const newPath = pathToDirectory + `/${file.slice(0, 7)}` + '.txt';
+      rename(
+        oldPath,
+        newPath,
+        err => console.log(err)
+      )
+    }
 }); 
